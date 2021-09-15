ff6840a3132578862eb33797fa3305ac823c74ca
cm <carloshrmoraes@gmail.com>
2021-09-15 11:10:35 -0300

    final screen styling

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,7 +1,9 @@
+import "../styles/Over.css";
+
 function GameOver({ scores, didLose }) {
   let result = didLose ? "You lost..." : "You won!!";
   return (
-    <div>
+    <div id="game_over">
       <h2>{result}</h2>
       <h3>You scored: {scores.current}</h3>
       <h3>Highest Score: {scores.highest}</h3>
